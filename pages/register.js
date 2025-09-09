27c1f826482e6417db69a9a9975175e6541bf748
Yoga Chandra Widjaya <yogachandraw@gmail.com>
2025-09-09 11:47:17 +0700

    update register.js

--- pages/register.js
+++ pages/register.js
@@ -17,7 +17,7 @@ export default function Register() {
     };
 
     try {
-      const res = await fetch("http://localhost:8080/api/auth/register", {
+      const res = await fetch("http://185.14.92.144:8080/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
